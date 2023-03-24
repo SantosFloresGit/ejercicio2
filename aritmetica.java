bcb752e878a1e1e82b6dab8c07f1fc3d07440e0a
Santos Jaime Flores Apaza <jsantos12391@gmail.com>
2023-03-24 15:15:01 -0400

    first commit suma

---
+++ aritmetica.java
@@ -0,0 +1,22 @@
+
+package sumar;
+import java.io.*;
+import java.util.Scanner;
+
+public class Sumar {
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        int num1,num2,sumar;
+        System.out.println("Ingrese Primer número:");
+        num1= teclado.nextInt();
+        System.out.println("Ingrese Segundo número:");
+        num2= teclado.nextInt();
+        
+        sumar=num1+num2;
+        
+        System.out.println("El resultado es:" +sumar);
+    }
+}
